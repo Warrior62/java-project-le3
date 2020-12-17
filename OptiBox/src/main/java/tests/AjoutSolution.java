1c7c602398468e34a90cbc226fa586f6208a0e1c
agathe <ag.pouillery@icloud.com>
2020-12-17 19:44:24 +0100

    Creation AjoutSolution

---
+++ OptiBox/src/main/java/tests/AjoutSolution.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+/**
+ *
+ * @author agpou
+ */
+public class AjoutSolution {
+    /**
+     * même principe que AjoutBDD()
+     * création d'une pile puis convertion en list pour pouvoir ajouter en bdd
+     * Soit un cast en list
+     * ou on parcout la pile et on met dans une liste
+     */
+}
